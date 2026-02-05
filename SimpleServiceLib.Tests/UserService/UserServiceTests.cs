ffcb0309c27ac78e9e89e644f6ef72e11f762245
Dmitry Petrichenko <dmitry.petrichenko@aristocrat.com>
2026-02-05 10:46:23 +0000

    update

---
+++ SimpleServiceLib.Tests/UserService/UserServiceTests.cs
@@ -0,0 +1,40 @@
+namespace SimpleServiceLib.Tests;
+
+public class UserServiceTests 
+    : TestsBase<UserServiceDriver, UserServiceVerifier>
+{
+    [Test]
+    public void UserLogin_Succeeds_WithValidCredentials()
+    {
+        // Act
+        Driver.LoginUser();
+
+        // Assert
+        Verifier.AssertLoginSuccess();
+    }
+}
+
+public class UserServiceDriver : TestContextAware
+{
+    private UserService _service = null!;
+
+    public void LoginUser()
+    {
+        var result = _service.Login("name", "pass");
+        Context.Results.Set("loginResult", result);
+    }
+
+    protected override void OnInit(TestContext ctx)
+    {
+        _service = new UserService(ctx.LogService);
+    }
+}
+
+public class UserServiceVerifier : TestContextAware
+{
+    public void AssertLoginSuccess()
+    {
+         var result = Context.Results.Get<string>("loginResult");
+         Assert.That(result, Is.EqualTo("ok"));
+    }
+}
---
+++ SimpleServiceLib/UserService.cs
@@ -0,0 +1,19 @@
+﻿using System.Dynamic;
+using SimpleServiceLib.Tests;
+
+namespace SimpleServiceLib;
+
+public sealed class UserService
+{
+    private readonly ILogService _logService;
+
+    public UserService(ILogService logService)
+    {
+        _logService = logService;
+    }
+
+    public string Login(string username, string password)
+    {
+        return "ok";
+    }
+}
